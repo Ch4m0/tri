bcad2e2646d19b436951528cc7f085e826988fe2
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-08-01 10:41:46 -0500

    add fix route login

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  Route,
-  Routes,
-} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import './index.css'
 import LoginPage from './LoginPage'
 
 const App = () => (
   <Routes>
-    <Route path="/login" element={<LoginPage />} />
+    <Route path="/" element={<LoginPage />} />
   </Routes>
 )
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+export default App
--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css'
 
-//const Login = React.lazy(() => import('login/App'))
-import LoginApp from 'login/App'
+const RemoteLoginApp = React.lazy(() => import('login/App'))
 
 const App = () => (
   <div className="container">
+    <h1>MAIn</h1>
     <Routes>
-      <Route path="/login" element={<LoginApp />}></Route>
+      <Route path="/" element={<h1>HOME</h1>}></Route>
+      <Route
+        path="/login/*"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <RemoteLoginApp />
+          </Suspense>
+        }
+      ></Route>
+      <Route path="*" element={<h1>404</h1>}></Route>
     </Routes>
   </div>
 )
